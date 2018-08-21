0f905217c39287dfaabdcc91bb1736ed1b1810bd
Caoyang Jiang <cjiang@mtu.edu>
2018-08-21 14:36:29 -0700

    increment to 0.0.2

--- hvr-codec-ios.rb
+++ hvr-codec-ios.rb
@@ -1,9 +1,9 @@
 class HvrCodecIos < Formula
   desc "hvr-codec for Ios"
   homepage "http://github.com/HypeVR/hvr-codec-ios"
-  url "https://github.com/HypeVR/hvr-codec-ios/archive/0.0.1.tar.gz", :using => :github_private_repo
-  sha256 "d8c3d32b1d4344bd4a21e6ea7e26540885f2bb5b7f0b4307bd62af580539c630"
-  head "http://github.com/HypeVR/hvr-codec-ios.git"
+  url "https://github.com/HypeVR/hvr-codec-ios/archive/0.0.2.tar.gz", :using => :github_private_repo
+  sha256 "6312dfe5e0dfe58b4275a7a88a64afb851d60fbfc416b8e2ffe6cdf8b146ad6a"
+  head "http://github.com/HypeVR/hvr-codec-ios.git", :using => :github_private_repo
 
   bottle do
     root_url "https://s3-us-west-2.amazonaws.com/hvr-codec-ios"
@@ -17,7 +17,7 @@ class HvrCodecIos < Formula
   def install
     Dir.mkdir("build")
     Dir.chdir("build")
-    system "cmake", "..", *std_cmake_args, "-DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake",  "-DIOS_PLATFORM=SIMULATOR64"
+    system "cmake", "..", *std_cmake_args, "-DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake",  "-DIOS_PLATFORM=OS"
     system "make", "install"
   end
 end
