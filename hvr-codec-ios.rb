2cd3ad2a0896fb734fd660aad419e613dbc8a97d
Caoyang Jiang <cjiang@mtu.edu>
2018-08-21 10:35:08 -0700

    add root url

--- hvr-codec-ios.rb
+++ hvr-codec-ios.rb
@@ -6,10 +6,11 @@ class HvrCodecIos < Formula
   head "http://github.com/HypeVR/hvr-codec-ios.git"
 
   bottle do
+    root_url "https://s3-us-west-2.amazonaws.com/hvr-codec-ios"
     cellar :any_skip_relocation
     sha256 "ad6f2622b2e19c04aca72b48969a8957ece69a924cbadfe5134e182c00aa48cb" => :high_sierra
   end
-  
+
   depends_on "cmake"     => :build
   depends_on "ffmpeg-ios"     => :build
 
