f65c8363c917e9c9a225d37cc92b0c8797ce709d
Caoyang Jiang <cjiang@mtu.edu>
2018-08-21 10:33:26 -0700

    add bottle

--- hvr-codec-ios.rb
+++ hvr-codec-ios.rb
@@ -5,6 +5,11 @@ class HvrCodecIos < Formula
   sha256 "d8c3d32b1d4344bd4a21e6ea7e26540885f2bb5b7f0b4307bd62af580539c630"
   head "http://github.com/HypeVR/hvr-codec-ios.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ad6f2622b2e19c04aca72b48969a8957ece69a924cbadfe5134e182c00aa48cb" => :high_sierra
+  end
+  
   depends_on "cmake"     => :build
   depends_on "ffmpeg-ios"     => :build
 
