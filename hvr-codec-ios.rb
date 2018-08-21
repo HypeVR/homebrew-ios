5997ef1b0192c8779e2f3a54b0a035c57dd18121
Caoyang Jiang <cjiang@mtu.edu>
2018-08-21 10:15:42 -0700

    - fix cmake options

--- hvr-codec-ios.rb
+++ hvr-codec-ios.rb
@@ -1,15 +1,17 @@
 class HvrCodecIos < Formula
   desc "hvr-codec for Ios"
   homepage "http://github.com/HypeVR/hvr-codec-ios"
-  url "https://github.com/HypeVR/hvr-codec-ios/archive/0.0.1.tar.gz"
-  sha256 "6361f7c32a79ef2ac5113cb4b38adbc8653d44ae2c9e0ddbd04b1998f0e44053"
+  url "https://github.com/HypeVR/hvr-codec-ios/archive/0.0.1.tar.gz", :using => :github_private_repo
+  sha256 "d8c3d32b1d4344bd4a21e6ea7e26540885f2bb5b7f0b4307bd62af580539c630"
   head "http://github.com/HypeVR/hvr-codec-ios.git"
 
   depends_on "cmake"     => :build
   depends_on "ffmpeg-ios"     => :build
 
   def install
-    system "cmake", ".", *std_cmake_args
+    Dir.mkdir("build")
+    Dir.chdir("build")
+    system "cmake", "..", *std_cmake_args, "-DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake",  "-DIOS_PLATFORM=SIMULATOR64"
     system "make", "install"
   end
 end
