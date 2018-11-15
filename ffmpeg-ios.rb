34d7f9a9d9abfb2efcaf2743e62894731a744957
Caoyang Jiang <cjiang@mtu.edu>
2018-11-14 18:20:59 -0800

    increase version

--- ffmpeg-ios.rb
+++ ffmpeg-ios.rb
@@ -1,7 +1,7 @@
 class FfmpegIos < Formula
   desc "Ffmpeg 4.0.3 for Ios"
   homepage "http://github.com/hypevr/ffmpeg-iOS"
-  url "https://github.com/hypevr/ffmpeg-iOS/archive/0.0.2.tar.gz"
+  url "https://github.com/hypevr/ffmpeg-iOS/archive/0.0.3.tar.gz"
   sha256 "54158d0c4fd4b85c516b3b607cfef0b1a1b583a39da60e575bc088dd87757068"
   head "http://github.com/hypevr/ffmpeg-iOS.git"
 
