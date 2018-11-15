5c2c49b42b9272be6961aac636ee79306bf2b069
Caoyang Jiang <cjiang@mtu.edu>
2018-11-14 18:21:44 -0800

    - fix md5

--- ffmpeg-ios.rb
+++ ffmpeg-ios.rb
@@ -2,7 +2,7 @@ class FfmpegIos < Formula
   desc "Ffmpeg 4.0.3 for Ios"
   homepage "http://github.com/hypevr/ffmpeg-iOS"
   url "https://github.com/hypevr/ffmpeg-iOS/archive/0.0.3.tar.gz"
-  sha256 "54158d0c4fd4b85c516b3b607cfef0b1a1b583a39da60e575bc088dd87757068"
+  sha256 "9e2181fce4371574e838567967fb8a2d268d1661c222c92d01f0edff6871e4d0"
   head "http://github.com/hypevr/ffmpeg-iOS.git"
 
   depends_on "cmake"     => :build
