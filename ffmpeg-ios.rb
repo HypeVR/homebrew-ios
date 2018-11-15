a8a1d72bce42f7696eefa1d08c1a2baa3ca82b60
Caoyang Jiang <cjiang@mtu.edu>
2018-11-14 18:05:17 -0800

    - fix sha256

--- ffmpeg-ios.rb
+++ ffmpeg-ios.rb
@@ -2,7 +2,7 @@ class FfmpegIos < Formula
   desc "Ffmpeg 4.0.3 for Ios"
   homepage "http://github.com/hypevr/ffmpeg-iOS"
   url "https://github.com/hypevr/ffmpeg-iOS/archive/0.0.2.tar.gz"
-  sha256 "82037430d6e5891e0f1b139d29b0c9824930c1ba3ebcdd2efd260894938f2992"
+  sha256 "54158d0c4fd4b85c516b3b607cfef0b1a1b583a39da60e575bc088dd87757068"
   head "http://github.com/hypevr/ffmpeg-iOS.git"
 
   depends_on "cmake"     => :build
