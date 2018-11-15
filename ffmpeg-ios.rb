86b0b961c9bc05a6f8c302266969329de89950b4
Caoyang Jiang <cjiang@mtu.edu>
2018-11-14 18:04:03 -0800

    - fix md5 for the archived ffmpeg

--- ffmpeg-ios.rb
+++ ffmpeg-ios.rb
@@ -1,7 +1,7 @@
 class FfmpegIos < Formula
   desc "Ffmpeg 4.0.3 for Ios"
   homepage "http://github.com/hypevr/ffmpeg-iOS"
-  url "https://github.com/hypevr/ffmpeg-iOS/archive/0.0.1.tar.gz"
+  url "https://github.com/hypevr/ffmpeg-iOS/archive/0.0.2.tar.gz"
   sha256 "82037430d6e5891e0f1b139d29b0c9824930c1ba3ebcdd2efd260894938f2992"
   head "http://github.com/hypevr/ffmpeg-iOS.git"
 
