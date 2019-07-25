aa7a1f8ea1b7524ce32de3223766a1f72e27878b
Caoyang Jiang <cjiang@mtu.edu>
2019-07-25 13:42:58 -0700

    - update sha256

--- boost-ios.rb
+++ boost-ios.rb
@@ -2,7 +2,7 @@ class BoostIos < Formula
   desc "boost 1.70 for iOS"
   homepage "http://github.com/hypevr/boost-iOS"
   url "https://github.com/HypeVR/boost-iOS/archive/0.0.1.tar.gz"
-  sha256 "fa08af8619f21e3b940c7df516238a4455eac36156b1e48b47f487f47c1df4d1"
+  sha256 "3ceaa4007b176563c536f33ef4b9a7f6b170711666e9886b893d71a7a53770a0"
   head "http://github.com/hypevr/boost-iOS.git"
 
   depends_on "cmake"     => :build
