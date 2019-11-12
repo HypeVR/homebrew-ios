42669bb38c27e99f9b13272712b2e40e2200b3b0
Caoyang Jiang <cjiang@mtu.edu>
2019-11-11 22:12:43 -0800

    - update sdl2 formula name

--- sdl2-ios.rb
+++ sdl2-ios.rb
@@ -1,5 +1,5 @@
-class FfmpegIos < Formula
-  desc "Ffmpeg 4.0.3 for Ios"
+class SDL2Ios < Formula
+  desc "SDL2 2.0.10 for Ios"
   homepage "http://github.com/hypevr/sdl2-iOS"
   url "https://github.com/hypevr/sdl2-ios/archive/0.0.1.tar.gz"
   sha256 "3c494144b28b9b1aa4b6b6ddc741da32900ee525efdc3fb8b24f30a9bd3c3ad6"
